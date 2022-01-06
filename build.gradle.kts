4ff941b7797ef46e4689d98822d4244814e6111a
Piotr Janicki <pcpiotr@gmail.com>
2022-01-06 12:16:10 +0100

    Package publish fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    jacoco
     kotlin("jvm") version "1.6.10"
     id("org.sonarqube") version "3.3"
-    jacoco
+    id("maven-publish")
 }
 
 group = "pl.redny"
@@ -24,9 +25,12 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(true)
+    }
 }
 
-tasks.named("sonarqube") {
+tasks.sonarqube {
     dependsOn(tasks.named("jacocoTestReport"))
 }
 
@@ -39,13 +43,25 @@ sonarqube {
         property("sonar.projectKey", "janicki-piotr_cqrs-kt")
         property("sonar.organization", "pcpiotr-github")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.core.codeCoveragePlugin","jacoco")
+        property("sonar.core.codeCoveragePlugin", "jacoco")
         property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
     }
 }
 
-tasks.jacocoTestReport {
-    reports {
-        xml.required.set(true)
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/janicki-piotr/cqrs-kt")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
     }
 }
